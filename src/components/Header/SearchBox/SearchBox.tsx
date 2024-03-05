cf2b0919404fef2a53d1250bfc5bbec69fe72183
Jangeunhye <dmsgp220@gmail.com>
2024-03-05 19:58:01 +0900

    chore: commitlint 설정

--- src/components/Header/SearchBox/SearchBox.tsx
+++ src/components/Header/SearchBox/SearchBox.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function SearchBox() {
   return (
-    <div className="w-[160px] h-[24px] relative">
+    <div className="w-40 h-6 relative">
       <input className="w-full bg-primary-20 rounded-[10px]" type="search" />
       <div className="absolute w-[15.22px] h-[15.22px] top-[3.75px] right-[8.03px]">
         <Image
