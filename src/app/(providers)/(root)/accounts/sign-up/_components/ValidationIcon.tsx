ba440cf92aa7fb35895d933570f3096dffb43673
baeseongjae <3574214@naver.com>
2024-03-07 00:44:40 +0900

    feat: 벨리데이션 아이콘 컴포넌트 생성 - 회원가입 입력값 유효성검사에 따라 조건부 렌더링

---
+++ src/app/(providers)/(root)/accounts/sign-up/_components/ValidationIcon.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+
+interface ValidationIconProps {
+  isValid: boolean;
+  focusedInput: string;
+  focusedInputName: string;
+}
+
+function ValidationIcon(props: ValidationIconProps) {
+  const { isValid, focusedInput, focusedInputName } = props;
+
+  return (
+    <>
+      {focusedInput === focusedInputName && (
+        <>
+          {isValid ? (
+            <Image
+              src="/assets/validation_valid.svg"
+              alt="체크표시"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <Image
+              src="/assets/validation_invalid.svg"
+              alt="X표시"
+              width={20}
+              height={20}
+            />
+          )}
+        </>
+      )}
+      {focusedInput !== focusedInputName && (
+        <>
+          {isValid ? (
+            <Image
+              src="/assets/validation_valid.svg"
+              alt="체크표시"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <Image
+              src="/assets/validation_default.svg"
+              alt="디폴트"
+              width={20}
+              height={20}
+            />
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+export default ValidationIcon;
