bced00eb8db9568c933977fa2cdbd586b48ca81d
HyoEun Sim <hyoeun5000@naver.com>
2024-03-08 01:58:27 +0900

    feat: input, textarea 유효성 검증 추가

--- src/components/Forms/InputForm/InputForm.tsx
+++ src/components/Forms/InputForm/InputForm.tsx
@@ -1,6 +1,7 @@
 import Icon from "@/components/Icon";
 import { PropsWithChildren } from "react";
 import { useFormContext } from "react-hook-form";
+import blueDot from "../../../../public/assets/blueDot.svg";
 import grayDot from "../../../../public/assets/grayDot.svg";
 import titleIcon from "../../../../public/assets/titleIcon.svg";
 import FormOuter from "../FormOuter";
@@ -15,18 +16,24 @@ type InputFormProps = PropsWithChildren<{
 function InputForm({ label, id, placeholder }: InputFormProps) {
   const {
     register,
+    watch,
     formState: { errors },
   } = useFormContext();
-  const errorMessages = errors[id] ? errors[id]!.message?.toString() : "";
-  const hasError = !!(errors && errorMessages);
 
   return (
     <FormOuter>
       <div className="flex items-center">
         <Label id={id} label={label} iconSrc={titleIcon} />
         <div className="flex pl-1 pb-2 items-center">
-          <Icon src={grayDot} alt="title-required-message" />
-          <p className="text-natural-50 text-xs pl-1">
+          <Icon
+            src={String(watch(id)).length >= 10 ? blueDot : grayDot}
+            alt="title-required-message"
+          />
+          <p
+            className={`text-natural-50 text-xs pl-1 ${
+              String(watch(id)).length >= 5 ? "text-primary-100" : ""
+            }`}
+          >
             제목은 5글자 이상이어야 합니다.
           </p>
         </div>
@@ -37,10 +44,9 @@ function InputForm({ label, id, placeholder }: InputFormProps) {
         type="text"
         aria-label={label}
         placeholder={placeholder}
-        className=" border border-natural-50 text-natural-50 text-sm rounded-lg focus:ring-primary-100 focus:border-primary-100 block w-full dark:bg-natural-50 dark:border-natural-50 dark:placeholder-natural-50 dark:text-white dark:focus:ring-primary-100 dark:focus:border-primary-100 px-5 py-3 font-light"
-        {...register(`${id}`, { required: `${placeholder}` })}
+        className=" border border-natural-50 placeholder:text-natural-50 text-sm rounded-lg focus:ring-primary-100 focus:border-primary-100 block w-full dark:bg-natural-50 dark:border-natural-50 dark:placeholder-natural-50 dark:text-white dark:focus:ring-primary-100 dark:focus:border-primary-100 px-5 py-3 font-light"
+        {...register(`${id}`, { required: true, minLength: 5 })}
       />
-      {/* {hasError && <p>{errorMessages}</p>} */}
     </FormOuter>
   );
 }
--- src/components/Forms/TextAreaForm/TextAreaForm.tsx
+++ src/components/Forms/TextAreaForm/TextAreaForm.tsx
@@ -1,6 +1,7 @@
 import Icon from "@/components/Icon";
 import { PropsWithChildren } from "react";
 import { useFormContext } from "react-hook-form";
+import blueDot from "../../../../public/assets/blueDot.svg";
 import contentIcon from "../../../../public/assets/contentIcon.svg";
 import grayDot from "../../../../public/assets/grayDot.svg";
 import FormOuter from "../FormOuter";
@@ -15,6 +16,7 @@ type TextareaFormProps = PropsWithChildren<{
 function TextareaForm({ label, id, placeholder }: TextareaFormProps) {
   const {
     register,
+    watch,
     formState: { errors },
   } = useFormContext();
   const errorMessages = errors[id] ? errors[id]!.message?.toString() : "";
@@ -25,8 +27,15 @@ function TextareaForm({ label, id, placeholder }: TextareaFormProps) {
       <div className="flex items-center">
         <Label id={id} label={label} iconSrc={contentIcon} />
         <div className="flex pl-1 pb-2">
-          <Icon src={grayDot} alt="title-required-message" />
-          <p className="text-natural-50 text-xs pl-1">
+          <Icon
+            src={String(watch(id)).length >= 10 ? blueDot : grayDot}
+            alt="title-required-message"
+          />
+          <p
+            className={`text-natural-50 text-xs pl-1 ${
+              String(watch(id)).length >= 10 ? "text-primary-100" : ""
+            }`}
+          >
             내용은 10글자 이상이어야 합니다.
           </p>
         </div>
@@ -35,8 +44,8 @@ function TextareaForm({ label, id, placeholder }: TextareaFormProps) {
         id={id}
         aria-label={label}
         placeholder={placeholder}
-        className=" border border-natural-50 text-natural-50 text-sm rounded-lg focus:ring-primary-100 focus:border-primary-100 block w-full dark:bg-natural-50 dark:border-natural-50 dark:placeholder-natural-50 dark:text-white dark:focus:ring-primary-100 dark:focus:border-primary-100 px-5 py-3 font-light resize-none  h-32 p-4"
-        {...register(`${id}`, { required: `${placeholder}` })}
+        className=" border border-natural-50 placeholder:text-natural-50 text-sm rounded-lg focus:ring-primary-100 focus:border-primary-100 block w-full dark:bg-natural-50 dark:border-natural-50 dark:placeholder-natural-50 dark:text-white dark:focus:ring-primary-100 dark:focus:border-primary-100 px-5 py-3 font-light resize-none h-32 p-4"
+        {...register(`${id}`, { required: true, minLength: 10 })}
       />
       {/* {hasError && <p>{errorMessages}</p>} */}
     </FormOuter>
