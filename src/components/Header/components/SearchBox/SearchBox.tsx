52254f9fa559fa7c67c531c8a8d477e10798987e
Jangeunhye <dmsgp220@gmail.com>
2024-03-08 23:55:01 +0900

    feat: 검색창 크기 수정

--- src/components/Header/components/SearchBox/SearchBox.tsx
+++ src/components/Header/components/SearchBox/SearchBox.tsx
@@ -4,15 +4,15 @@ function SearchBox() {
   return (
     <div className="relative h-full flex items-center">
       <input
-        className="w-full bg-primary-20 rounded-[10px] h-full"
+        className="w-full bg-primary-20 rounded-[20px] h-full"
         type="search"
       />
       <div className="absolute right-2 sm:top-1.8 sm:right-3 ">
         <Image
           src="/assets/search.svg"
           layout="responsive"
-          width={1}
-          height={1}
+          width={20}
+          height={20}
           alt="search-icon"
         />
       </div>
