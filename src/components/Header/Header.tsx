0a266ad9eeb34d879435d2f98857118f15ff538e
Jangeunhye <dmsgp220@gmail.com>
2024-03-08 23:49:09 +0900

    feat: 헤더shadow 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,19 @@ import SearchBox from "./components/SearchBox";
 
 function Header() {
   return (
-    <header className="px-5 py-4 mx-auto max-w-screen-md flex w-full items-center justify-between h-16 sm:h-20">
-      <Link href="/" className="font-bold text-xl">
-        Sweatier
-      </Link>
-      <div className="flex-grow mx-3 sm:mx-7 h-[65%] items-center">
-        <SearchBox />
-      </div>
+    <header className="border shadow-sm">
+      <div className="px-5 py-4 mx-auto max-w-screen-md flex w-full items-center justify-between h-16">
+        <Link href="/" className="font-bold text-xl">
+          Sweatier
+        </Link>
+        <div className="flex-grow mx-4 sm:mx-20 sm:h-8 h-full">
+          <SearchBox />
+        </div>
 
-      <button className="font-bold w-14 text-sm text-primary-100">
-        로그인
-      </button>
+        <button className="font-bold w-14 text-sm text-primary-100">
+          로그인
+        </button>
+      </div>
     </header>
   );
 }
