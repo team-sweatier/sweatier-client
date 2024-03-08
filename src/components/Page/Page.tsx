2d4c3897c5dd96837efcc8581694f972466b936d
HyoEun Sim <hyoeun5000@naver.com>
2024-03-08 11:30:14 +0900

    style: page component의 data full width 속성 삭제

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 function Page({ children }: { children: React.ReactNode }) {
   return (
-    <main className="px-5 py-4 mx-auto max-w-screen-lg data-[full-width=true]:max-w-none flex flex-col w-full items-center justify-start">
+    <main className="px-5 py-4 mx-auto max-w-screen-lg flex flex-col w-full items-center justify-start">
       {children}
     </main>
   );
