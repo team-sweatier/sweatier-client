9bb86de7f2b854696940e8d6f8f1738477f3ce88
baeseongjae <3574214@naver.com>
2024-03-10 02:10:50 +0900

    chore: 백엔드 api 서버 주소 업데이트

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import authAPI from "./auth/auth.api";
 
 export const client = axios.create({
-  baseURL:
-    "https://port-0-sweatier-server-v2-dc9c2nltdolabq.sel5.cloudtype.app",
+  baseURL: "https://sweatier-server-he2ntmjbhq-du.a.run.app",
   withCredentials: true,
 });
 
