d6bb90ee6b9a1dad810ffb37ce4a7768c65046ed
baeseongjae <3574214@naver.com>
2024-03-10 01:05:19 +0900

    fix: 모달창 z-index 값 증가로 최상위에 위치시킴

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,7 +13,7 @@ function Modal({ children, className }: PropsWithChildren<ModalProps>) {
 
   return (
     <div
-      className="bg-black/50 flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 z-20"
+      className="bg-black/50 flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 z-50"
       onClick={handleClickBackdrop}
     >
       <div
