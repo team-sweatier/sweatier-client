578b1db9237ad36e06a556cb82ea526661d5c3d6
HyoEun Sim <hyoeun5000@naver.com>
2024-03-09 22:54:57 +0900

    refactor: input component register import

--- src/components/Forms/Input/Input.tsx
+++ src/components/Forms/Input/Input.tsx
@@ -11,21 +11,21 @@ type InputProps = PropsWithChildren<{
 
 function Input({
   id,
-  label,
   placeholder,
   minLength,
   className,
   ...props
 }: InputProps) {
   const { register } = useFormContext();
+
   return (
     <input
+      {...register(id, { required: true, minLength: minLength })}
       id={id}
       type="text"
-      aria-label={label}
+      aria-label={id}
       placeholder={placeholder}
       className={`border border-natural-30 placeholder:text-natural-50 text-sm rounded-lg focus:ring-primary-100 focus:border-primary-100 block w-full dark:bg-natural-50 dark:border-natural-50 dark:placeholder-natural-50 dark:text-white dark:focus:ring-primary-100 dark:focus:border-primary-100 px-5 py-3 font-light ${className}`}
-      {...register(`${id}`, { required: true, minLength: minLength })}
       {...props}
     />
   );
