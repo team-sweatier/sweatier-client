2613f0f5703e131f95c45c1401bfd8e401e18987
baeseongjae <3574214@naver.com>
2024-03-09 20:26:38 +0900

    feat: 로그인 모달 레이아웃 및 디자인

--- src/app/(providers)/(root)/accounts/_components/LogInModal/LogInModal.tsx
+++ src/app/(providers)/(root)/accounts/_components/LogInModal/LogInModal.tsx
@@ -1,49 +1,90 @@
 "use client";
 
+import Heading from "@/components/Heading";
 import Modal from "@/components/Modal";
+import { useModalStore } from "@/store";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 function LogInModal() {
+  const modal = useModalStore();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const handleClickLogIn = () => {};
 
+  const handleClickGoToSignUp = () => {
+    modal.close();
+  };
+
   return (
     <Modal>
-      <h2>로그인</h2>
+      <Heading className="text-center">로그인</Heading>
       <form
         action=""
         onSubmit={(e) => e.preventDefault()}
-        className="flex flex-col gap-y-4"
+        className="flex flex-col gap-y-4 px-5 py-5"
       >
         <ul className="flex flex-col gap-y-4">
           <li className="flex flex-col">
-            <label htmlFor="email">이메일</label>
+            <label htmlFor="email" className="text-neutral-70">
+              이메일
+            </label>
             <input
               type="text"
               id="email"
-              className="h-12 border border-slate-300 focus:border-black outline-none transition rounded-md pl-4"
+              className="h-10 border text-neutral-70 border-slate-300 focus:border-black outline-none transition rounded-lg pl-4"
               value={email}
+              placeholder="이메일을 입력해주세요."
               onChange={(e) => setEmail(e.target.value)}
             />
           </li>
           <li className="flex flex-col">
-            <label htmlFor="password">비밀번호</label>
+            <label htmlFor="password" className="text-neutral-70">
+              비밀번호
+            </label>
             <input
               type="password"
               id="password"
-              className="h-12 border border-slate-300 focus:border-black outline-none transition rounded-md pl-4"
+              className="h-10 border text-neutral-70 border-slate-300 focus:border-black outline-none transition rounded-lg pl-4"
               value={password}
+              placeholder="비밀번호를 입력해주세요."
               onChange={(e) => setPassword(e.target.value)}
             />
           </li>
         </ul>
         <button
           onClick={handleClickLogIn}
-          className="bg-purple-700 text-white font-semibold h-12 mt-10 transition hover:-translate-y-1 active:translate-y-0"
+          className="h-10 bg-primary-100 text-white rounded-lg font-semibold my-4 transition hover:-translate-y-1 active:translate-y-0"
         >
-          로그인하기
+          로그인
         </button>
+        <div className="flex items-center justify-center space-x-2">
+          <div className="flex-1 border-t-2 border-dotted border-neutral-40"></div>
+          <span className="px-2 text-xs text-neutral-70 bg-white">
+            간편 로그인
+          </span>
+          <div className="flex-1 border-t-2 border-dotted border-neutral-40"></div>
+        </div>
+        <button className="h-10 bg-yellow-300 w-full px-6 rounded-lg font-semibold my-4 transition hover:-translate-y-1 active:translate-y-0">
+          <div className="h-full flex items-center justify-center">
+            <Image
+              src="/assets/kakaotalk.svg"
+              alt="카카오톡"
+              width={24}
+              height={24}
+            />
+            <span>카카오로 계속하기</span>
+          </div>
+        </button>
+        <Link
+          href="/accounts/sign-up"
+          onClick={handleClickGoToSignUp}
+          className="text-primary-100 text-xs text-center"
+        >
+          아직 회원이 아니신가요?{" "}
+          <span className="underline font-bold">회원가입</span>
+        </Link>
       </form>
     </Modal>
   );
