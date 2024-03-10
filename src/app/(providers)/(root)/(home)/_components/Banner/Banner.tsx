7bb3a127859c7492b62154a33a88640335bff0d1
Jangeunhye <dmsgp220@gmail.com>
2024-03-11 02:26:28 +0900

    feat: 이미지캐러셀, 날짜 arrow 버튼 추가

--- src/app/(providers)/(root)/(home)/_components/Banner/Banner.tsx
+++ src/app/(providers)/(root)/(home)/_components/Banner/Banner.tsx
@@ -1,16 +1,56 @@
-import Image from "next/legacy/image";
+"use client";
+import Image from "next/image";
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Swiper, SwiperSlide } from "swiper/react";
 function Banner() {
   return (
-    <div className="relative w-full mb-5 ">
-      <Image
-        src="/assets/carousel-image-1.svg"
-        sizes="100vw"
-        priority
-        width={350}
-        height={114}
-        alt="carousel-image"
-      />
+    <div className="w-full mb-5">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        // navigation
+
+        autoplay
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <Image
+            src="/assets/carousel-image-1.svg"
+            layout="responsive"
+            width={350}
+            height={114}
+            alt="carousel-image"
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/assets/carousel-image-1.svg"
+            layout="responsive"
+            width={350}
+            height={114}
+            alt="carousel-image"
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Image
+            src="/assets/carousel-image-1.svg"
+            layout="responsive"
+            width={350}
+            height={114}
+            alt="carousel-image"
+            loading="lazy"
+          />
+        </SwiperSlide>{" "}
+      </Swiper>
     </div>
   );
 }
--- src/app/(providers)/(root)/(home)/_components/WeeklyCalendar/WeeklyCalendar.tsx
+++ src/app/(providers)/(root)/(home)/_components/WeeklyCalendar/WeeklyCalendar.tsx
@@ -1,32 +1,65 @@
-"use client";
-
-import useFilterStore from "@/store/filter.store";
-import { Dayjs } from "dayjs";
-import { useEffect, useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import Day from "../Day";
 
 function WeeklyCalendar() {
-  const { date, setDate } = useFilterStore();
-  const [weeksList, setWeeksList] = useState<Dayjs[]>([]);
+  const [weeksList, setWeeksList] = useState<Dayjs[]>();
+  const scrollContainerRef = useRef<HTMLUListElement>(null); // HTMLUListElement 타입 지정
 
   useEffect(() => {
-    let tempWeeksList = [date];
+    let tempWeeksList = [dayjs()];
     for (let i = 0; i < 13; i++) {
       tempWeeksList.push(tempWeeksList[tempWeeksList.length - 1].add(1, "day"));
     }
     setWeeksList(tempWeeksList);
   }, []);
 
+  const scroll = (scrollOffset: number) => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += scrollOffset;
+    }
+  };
+
   return (
-    <div className="mb-4  flex justify-center sticky z-30 top-0 bg-white">
-      <ul className="scrollbar-hide overflow-x-auto flex border-b gap-x-4 ">
-        {weeksList.map((day) => (
-          <li key={day.toString()}>
-            <Day day={day} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {weeksList ? (
+        <div className="mb-4 flex items-center justify-center sticky z-30 top-0 bg-white h-14 ">
+          <button
+            onClick={() => scroll(-100)}
+            className="px-2 border-b border-transparent flex items-center"
+          >
+            <Image
+              src={`/assets/arrow-point.png`}
+              width={20}
+              height={20}
+              alt="left-arrow"
+              className="-rotate-180 mb-1"
+            />
+          </button>
+          <ul
+            ref={scrollContainerRef}
+            className="scrollbar-hide overflow-x-auto flex border-b gap-x-4 items-center"
+          >
+            {weeksList.map((day, index) => (
+              <li key={day.toString()} className="flex items-center">
+                <Day day={day} />
+              </li>
+            ))}
+          </ul>
+
+          <button onClick={() => scroll(100)} className="px-2 ">
+            <Image
+              src={`/assets/arrow-point.png`}
+              width={20}
+              height={20}
+              alt="right-arrow"
+              className="mb-1"
+            />
+          </button>
+        </div>
+      ) : null}
+    </>
   );
 }
 
