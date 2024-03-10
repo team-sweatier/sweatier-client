9083e05cfdc71276f1dc3e498e19fcc2c8662c1e
HyoEun Sim <hyoeun5000@naver.com>
2024-03-10 23:43:59 +0900

    feat: applybutton 구현

--- src/app/(providers)/(root)/matches/[matchId]/page.tsx
+++ src/app/(providers)/(root)/matches/[matchId]/page.tsx
@@ -1,11 +1,11 @@
+import ApplyStateButton from "./_components/ApplyStateButton";
 import Background from "./_components/Background";
 import MatchUpContainer from "./_components/MatchUpContainer";
 import OverlayBackground from "./_components/OverlayBackground";
-import ApplyStateButton from "./_components/applyStateButton";
 
 function MatchDetailPage() {
   return (
-    <main className="px-5 pb-[50px] mx-auto max-w-screen-md flex flex-col w-full items-center justify-start h-screen relative">
+    <main className="pb-[50px] mx-auto max-w-screen-md flex flex-col w-full items-center justify-start h-screen relative">
       <Background>
         <OverlayBackground>
           <div>
