f3ed350b92307460d389e163a64225b6937a507d
HyoEun Sim <hyoeun5000@naver.com>
2024-03-10 13:44:40 +0900

    fix: script load 위치 creatematchpage -> htmllayout으로 변경

--- src/app/(providers)/(root)/match/create/page.tsx
+++ src/app/(providers)/(root)/match/create/page.tsx
@@ -1,20 +1,15 @@
 import MatchForm from "@/components/Forms/MatchForm/MatchForm";
 import Heading from "@/components/Heading";
 import Page from "@/components/Page";
-import { KAKAO_SDK_URL } from "@/config";
-import Script from "next/script";
 
 const CreateMatchPage = () => {
   return (
-    <body>
-      <Page>
-        <div className="w-full max-w-screen-md">
-          <Heading>게시물 작성</Heading>
-          <MatchForm />
-        </div>
-      </Page>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-    </body>
+    <Page>
+      <div className="w-full relative">
+        <Heading>게시물 작성</Heading>
+        <MatchForm />
+      </div>
+    </Page>
   );
 };
 
