5f61b47d25d56874efd22c1c8c5967e687ec2017
HyoEun Sim <hyoeun5000@naver.com>
2024-03-11 17:48:54 +0900

    chore: kakamapform 주석 제거

--- src/components/Forms/KakaoMapForm/KakaoMapForm.tsx
+++ src/components/Forms/KakaoMapForm/KakaoMapForm.tsx
@@ -8,29 +8,23 @@ import Input from "../Input";
 import KakaoMap from "../KakaoMap";
 import Label from "../Label";
 
-type addressData = {
+type AddressData = {
   address: string;
 };
 
 function KakaoMapForm() {
-  const { control, watch } = useForm<addressData>();
-  // const { control, watch } = useFormContext();
-  const [searchKeyword, setSearchKeyword] = useState<string>("웅진IT 본사"); //* 입력한 키워드 값 (초기값 : 웅진 본사)
+  const { control, watch } = useForm<AddressData>();
+  const [searchKeyword, setSearchKeyword] = useState<string>("웅진IT 본사");
   const keyword = watch("address");
 
-  // "검색 버튼" 클릭 시 실행될 함수
   const handleSearch = () => {
-    setSearchKeyword(keyword);
+    setSearchKeyword(keyword || "웅진IT 본사");
   };
 
   return (
     <FormOuter>
-      <Label
-        id={"place"}
-        label={"경기장 위치"}
-        iconSrc={matchCreateIcons.place}
-      />
-      <div className="grid grid-rows-2 relative rounded-[10px] px-5 border-2 border-natural-20 w-full  h-[315px] sm:h-[480px]">
+      <Label id="place" label="경기장 위치" iconSrc={matchCreateIcons.place} />
+      <div className="grid grid-rows-2 relative rounded-[10px] px-5 border-2 border-natural-20 w-full h-[315px] sm:h-[480px]">
         <div className="grid grid-cols-4 items-center gap-x-4 w-full">
           <Controller
             control={control}
@@ -39,14 +33,14 @@ function KakaoMapForm() {
               <Input
                 id="address"
                 label="경기장 위치"
-                {...field}
                 placeholder="경기장명을 입력하세요."
                 className="sm:h-11 h-9 col-span-3"
+                {...field}
               />
             )}
           />
           <BlueButton
-            buttonLabel={"검색"}
+            buttonLabel="검색"
             isValid={!!keyword}
             onclick={handleSearch}
           />
