553d630262cf442010fc8d445f788241a0a67451
baeseongjae <3574214@naver.com>
2024-03-12 02:50:45 +0900

    feat: 내 티어 조회 api 호출 및 데이터 렌더링

--- src/app/(providers)/(root)/my-page/_components/Hexagon.tsx
+++ src/app/(providers)/(root)/my-page/_components/Hexagon.tsx
@@ -1,8 +1,11 @@
-function Hexagon({ label }: { label: string }) {
+function Hexagon({ sportType, tier }: { sportType: string; tier: string }) {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-20 h-24 bg-neutral-20 hover:bg-neutral-30 text-center flex items-end justify-center hexagon">
-        <span className="text-xs text-neutral-70 font-black mb-4">{label}</span>
+      <div className="w-20 h-24 bg-neutral-20 hover:bg-neutral-30 text-center flex flex-col items-center justify-between hexagon">
+        <span className="text-xs text-neutral-70 mt-10">{tier}</span>
+        <span className="text-xs text-neutral-70 font-black mb-4">
+          {sportType}
+        </span>
       </div>
     </div>
   );
