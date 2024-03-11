68f217d38b493f888a7cd0483b1b06fe7f4994d4
baeseongjae <3574214@naver.com>
2024-03-11 21:57:02 +0900

    fix: next 이미지 config 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "https://sweatier-server-he2ntmjbhq-du.a.run.app"],
+    domains: ["localhost", "sweatier-server-he2ntmjbhq-du.a.run.app"],
   },
   reactStrictMode: true,
 };
