bc1a622abb104ebf29c26b8c6a153ebd85cce732
baeseongjae <3574214@naver.com>
2024-03-11 21:14:44 +0900

    chore: next 이미지 config파일 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["localhost", "https://sweatier-server-he2ntmjbhq-du.a.run.app"],
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
