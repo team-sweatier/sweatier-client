671e2179232dce038f1f1c395f73a837cdaa0483
HyoEun Sim <hyoeun5000@naver.com>
2024-03-11 22:23:05 +0900

    style: userpostcontrolbutton text-color white로 변경

--- src/app/(providers)/(root)/matches/[matchId]/_components/UserPostControlButtons.tsx
+++ src/app/(providers)/(root)/matches/[matchId]/_components/UserPostControlButtons.tsx
@@ -29,7 +29,7 @@ function UserPostControlButtons({ matchId }: UserPostControlButtonsProps) {
         <button
           key={`${button.label}-${i}`}
           type="button"
-          className="text-neutral-70 font-medium rounded-full text-sm px-2 text-center cursor-pointer w-full border-2 border-natural-60  py-2 max-w-16 hover:bg-primary-100 hover:text-neutral-20 hover:border-primary-100 transition-colors"
+          className="text-neutral-70 font-medium rounded-full text-sm px-2 text-center cursor-pointer w-full border-2 border-natural-60  py-2 max-w-16 hover:bg-primary-100 hover:text-white hover:border-primary-100 transition-colors"
           onClick={button.onClick}
         >
           {button.label}
