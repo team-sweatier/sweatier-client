d21b183793fd9ca4dd7380cf836ea554c310d7b4
baeseongjae <3574214@naver.com>
2024-03-12 02:57:23 +0900

    feat: 신청내역, 경기내역 조회 버튼 클릭 이벤트 함수

--- src/app/(providers)/(root)/my-page/_components/ButtonToHistory.tsx
+++ src/app/(providers)/(root)/my-page/_components/ButtonToHistory.tsx
@@ -1,6 +1,15 @@
-function ButtonToHistory({ label }: { label: string }) {
+import { ComponentProps } from "react";
+
+interface ButtonToHistoryProps extends ComponentProps<"button"> {
+  label: string;
+}
+
+function ButtonToHistory({ label, ...props }: ButtonToHistoryProps) {
   return (
-    <button className="rounded-lg border border-neutral-50 w-1/2 h-12 font-bold hover:text-primary-100 hover:border-primary-100">
+    <button
+      {...props}
+      className="rounded-lg border border-neutral-50 w-1/2 h-12 font-bold hover:text-primary-100 hover:border-primary-100"
+    >
       {label}
     </button>
   );
--- src/app/(providers)/(root)/my-page/page.tsx
+++ src/app/(providers)/(root)/my-page/page.tsx
@@ -3,6 +3,7 @@
 import Heading from "@/components/Heading";
 import Page from "@/components/Page";
 import { useModalStore } from "@/store";
+import { useRouter } from "next/navigation";
 import ButtonToHistory from "./_components/ButtonToHistory";
 import MyProfileSection from "./_components/MyProfileSection";
 import MyTierSection from "./_components/MyTierSection";
@@ -10,9 +11,18 @@ import TiersInformationModal from "./_components/TiersInformationModal";
 
 function MyPage() {
   const modal = useModalStore();
+  const router = useRouter();
 
   const handleClickInformationButton = () => {
     modal.open(<TiersInformationModal />);
+  };
+
+  const handleClickButtonToMyApplication = () => {
+    router.push("my-page/my-applications");
+  };
+
+  const handleClickButtonToMyMatches = () => {
+    router.push("my-page/my-matches");
   };
 
   return (
@@ -26,8 +36,14 @@ function MyPage() {
         <MyTierSection onClickTierModal={handleClickInformationButton} />
       </section>
       <section className="w-full flex justify-between gap-x-5 pt-6">
-        <ButtonToHistory label="신청 내역 조회" />
-        <ButtonToHistory label="경기 내역 조회" />
+        <ButtonToHistory
+          label="신청 내역 조회"
+          onClick={handleClickButtonToMyApplication}
+        />
+        <ButtonToHistory
+          label="경기 내역 조회"
+          onClick={handleClickButtonToMyMatches}
+        />
       </section>
     </Page>
   );
