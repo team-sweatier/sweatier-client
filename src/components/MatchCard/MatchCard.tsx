a443fc1a96c3795400446f0755a16927bf56b7c4
Jangeunhye <dmsgp220@gmail.com>
2024-03-12 14:08:45 +0900

    feat: match type 추가

--- src/components/MatchCard/MatchCard.tsx
+++ src/components/MatchCard/MatchCard.tsx
@@ -1,10 +1,11 @@
 import AvailabilityButton from "@/components/Buttons/AvailabilityButton";
+import { Match } from "@/types/Match.type";
 
 import getMatchAvailableInfo from "@/utils/getMatchAvailableInfo";
 import translateGender from "@/utils/translateGender";
 import Link from "next/link";
 
-function MatchCard({ match, className }: { match: any; className: string }) {
+function MatchCard({ match, className }: { match: Match; className: string }) {
   const teamCapability = match.capability / 2;
   const gender = translateGender(match.gender);
 
