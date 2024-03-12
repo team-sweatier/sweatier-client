795fecdec178442cb7c771bbe5f4d4b5fdc7e8a3
HyoEun Sim <hyoeun5000@naver.com>
2024-03-12 19:45:13 +0900

    style: calendar css 수정

--- src/app/(providers)/(root)/my-page/(my-menu)/_components/MyCalendar.tsx
+++ src/app/(providers)/(root)/my-page/(my-menu)/_components/MyCalendar.tsx
@@ -16,21 +16,22 @@ function MyCalendar() {
       selected={selected}
       onSelect={(date) => setSelected(date)}
       fromDate={new Date()}
-      className="text-neutral-20"
+      className=""
       showOutsideDays
       classNames={{
-        table: "w-full border-collapse",
-        head_row: "flex font-medium text-gray-900",
+        table: "w-full",
+        head_row:
+          "flex font-medium text-gray-900 border-neutral-20 border-b-[0.5px]",
         head_cell: "m-1 w-full font-normal text-sm",
-        row: "w-full mt-2 border-neutral-20 border-b-[0.5px]",
-        cell: "text-gray-600 rounded-md h-9 w-9 text-center text-base p-0 m-1 mb-2 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-natural-40 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-primary-40 rounded-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        row: "w-full py-1 border-neutral-20 border-b-[0.5px]",
+        cell: "text-gray-600 rounded-md h-full w-full text-center text-base m-1 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day_range_end)]:bg-natural-40 [&:has([aria-selected].day-outside)]:text-white rounded-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 hover:rounded-md",
         day: "h-9 w-9 p-0 font-normal",
         day_range_end: "day-range-end",
         day_selected:
-          "rounded-md bg-primary-100 text-white hover:bg-primary-60 hover:text-white focus:bg-primary-100 focus:text-white text-center",
-        day_today: "text-primary-100 bg-red-400",
+          "rounded-md bg-primary-100 text-white hover:bg-primary-80 hover:text-white focus:bg-primary-100 focus:text-white text-center",
+        day_today: "text-primary-100 bg-green-200",
         day_outside:
-          "day-outside text-natural-40 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10 cursor-not-allowed",
+          "text-natural-40 opacity-20 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-0",
         day_disabled: "text-natural-40 cursor-not-allowed",
         day_hidden: "invisible",
       }}
--- src/app/(providers)/(root)/my-page/(my-menu)/my-matches/page.tsx
+++ src/app/(providers)/(root)/my-page/(my-menu)/my-matches/page.tsx
@@ -3,7 +3,7 @@ import MyMatchLists from "../_components/MyMatchLists";
 
 function MyMatchesPage() {
   return (
-    <main className="px-5 sm:px-0 pt-[30px] pb-[50px] mx-auto max-w-screen-md flex flex-col w-full justify-between">
+    <main className="px-5 sm:px-0 pt-[30px] pb-[50px] mx-auto max-w-screen-md w-full justify-between h-full">
       <div className="flex flex-col sm:grid grid-cols-2">
         <div className="w-full grow">
           <MyCalendar />
