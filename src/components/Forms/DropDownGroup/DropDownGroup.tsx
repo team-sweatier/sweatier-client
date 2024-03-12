86e31187dff7a424c9753c38c8c3cc22489a1477
HyoEun Sim <hyoeun5000@naver.com>
2024-03-12 13:10:29 +0900

    style: dropdowngroup flex 정렬

--- src/components/Forms/DropDownGroup/DropDownGroup.tsx
+++ src/components/Forms/DropDownGroup/DropDownGroup.tsx
@@ -10,36 +10,43 @@ function DropDownGroup() {
 
   return (
     <FormOuter>
-      <div className="flex gap-x-8 items-center">
-        <div className="flex items-center gap-x-2">
-          <Label id="hour" label="시간" iconSrc={matchCreateIcons.time} />
-          <Controller
-            control={control}
-            name="hour"
-            render={({ field }) => (
-              <DropDownBox
-                options={matchTypes.timeHours}
-                value={field.value}
-                onChange={field.onChange}
-              />
-            )}
-          />
-          <span>시</span>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <Label id="minute" label="분" iconSrc={matchCreateIcons.time} />
-          <Controller
-            control={control}
-            name="minute"
-            render={({ field }) => (
-              <DropDownBox
-                options={matchTypes.timeMinutes}
-                value={field.value}
-                onChange={field.onChange}
-              />
-            )}
-          />
-          <span>분</span>
+      <div className="flex flex-col justify-start ">
+        <Label
+          id="hour"
+          label="경기 시작 시간"
+          iconSrc={matchCreateIcons.time}
+        />
+
+        <div className="flex items-center gap-x-6 pt-1">
+          <div className="flex items-center gap-x-2">
+            <Controller
+              control={control}
+              name="hour"
+              render={({ field }) => (
+                <DropDownBox
+                  options={matchTypes.timeHours}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
+            <span>시</span>
+          </div>
+
+          <div className="flex items-center gap-x-2">
+            <Controller
+              control={control}
+              name="minute"
+              render={({ field }) => (
+                <DropDownBox
+                  options={matchTypes.timeMinutes}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
+            <span>분</span>
+          </div>
         </div>
       </div>
     </FormOuter>
