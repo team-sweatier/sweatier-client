fed3f86df7b0ba2150c1bd1f229e125f40586453
HyoEun Sim <hyoeun5000@naver.com>
2024-03-13 03:32:00 +0900

    feat: match 생성 api 구현

--- src/api/match/match.api.ts
+++ src/api/match/match.api.ts
@@ -1,4 +1,5 @@
 import { client } from "..";
+import { MatchDto } from "./match.dto";
 
 async function getMatches(sportType: string, date: string, region?: string) {
   const response = await client.get("/matches", {
@@ -32,9 +33,25 @@ async function getMatchesByTitle(keywords: string) {
   return matches;
 }
 
+async function createMatch(matchDto: MatchDto) {
+  const response = await client.post(`/matches`, {
+    data: {
+      matchDto,
+    },
+  });
+
+  const data = response.data;
+  if (!data.success) throw new Error(data.error.message);
+
+  const matches = data.result;
+
+  return matches;
+}
+
 const matchAPI = {
   getMatches,
   getMatchesByTitle,
+  createMatch,
 };
 
 export default matchAPI;
--- src/api/match/match.dto.ts
+++ src/api/match/match.dto.ts
@@ -1,1 +1,13 @@
-export type MatchDto = {};
+export type MatchDto = {
+  title: string;
+  content: string;
+  gender: "male" | "female" | "both";
+  capability: number;
+  sportsTypeName: string;
+  latitude: number;
+  longitude: number;
+  placeName: string;
+  region: string;
+  address: string;
+  matchDay: Date;
+};
