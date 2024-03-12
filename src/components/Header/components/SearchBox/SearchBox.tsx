893a7464a8567fdc2ee584253dce4b18b17cd2a2
Jangeunhye <dmsgp220@gmail.com>
2024-03-13 01:53:55 +0900

    fix: 검색창 warning 해결

--- src/components/Header/components/SearchBox/SearchBox.tsx
+++ src/components/Header/components/SearchBox/SearchBox.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 function SearchBox() {
-  const [keywords, setKeywords] = useState<string>();
+  const [keywords, setKeywords] = useState<string>("");
   const router = useRouter();
   const handleActiveEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.keyCode === 13) {
+    if (e.key === "Enter") {
       router.push(`search?keywords=${keywords}`);
     }
   };
   return (
     <div className="relative h-full flex items-center">
       <input
-        className="w-full bg-primary-20 rounded-[20px] h-full px-6"
+        className="w-full bg-primary-20 rounded-[20px] h-full px-5 text-sm"
         type="search"
         value={keywords}
         onChange={(e) => setKeywords(e.target.value)}
@@ -22,7 +22,7 @@ function SearchBox() {
       />
       <Link
         href={`search?keywords=${keywords}`}
-        className="absolute right-2 sm:top-1.8 sm:right-3 "
+        className="absolute right-2 sm:top-1.8 sm:right-3 hover:opacity-65"
       >
         <Image src={search} width={22} height={22} alt="search-icon" />
       </Link>
