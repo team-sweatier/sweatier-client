6bf75de811b812999929f963b1ff234ad7fcd933
Jangeunhye <dmsgp220@gmail.com>
2024-03-14 00:34:51 +0900

    fix: 검색창 리다이렉트 해결

--- src/app/(providers)/(root)/my-page/my-matches/[matchId]/rating-check/_components/RatingCheckCardsList/RatingCheckCardsList.tsx
+++ src/app/(providers)/(root)/my-page/my-matches/[matchId]/rating-check/_components/RatingCheckCardsList/RatingCheckCardsList.tsx
@@ -7,7 +7,7 @@ function RatingCheckCardsList({ myRatings }: { myRatings: MyRatings[] }) {
       <h2 className=" text-base w-full mb-4 font-bold text-natural-90">
         내가 받은 티어평가
       </h2>
-      <ul className="flex flex-col gap-y-4 sm:grid sm:grid-cols-2 sm:gap-x-7">
+      <ul className="flex flex-col gap-y-4 sm:gap-y-7 sm:grid sm:grid-cols-2 sm:gap-x-7">
         {myRatings.map((rating, index) => (
           <li key={rating.raterId}>
             <RatingCheckCard rating={rating} index={index} />
