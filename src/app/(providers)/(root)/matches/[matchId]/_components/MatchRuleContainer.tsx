0fe7ca9359cc126f25939c9922f46a2c6b18444e
HyoEun Sim <hyoeun5000@naver.com>
2024-03-13 19:06:03 +0900

    style: leading 행간 넓이 6 -> 7로 변경

--- src/app/(providers)/(root)/matches/[matchId]/_components/MatchRuleContainer.tsx
+++ src/app/(providers)/(root)/matches/[matchId]/_components/MatchRuleContainer.tsx
@@ -11,7 +11,7 @@ function MatchRuleContainer({ sportRules }: MatchRuleContainerProps) {
   return (
     <div className="border border-natural-30 rounded-lg  block w-full dark:bg-natural-50 dark:border-natural-50 dark:text-white p-5 mb-8">
       <div className="text-[16px] font-bold">경기규칙</div>
-      <ul className="py-4 text-[14px] text-neutral-90 leading-6 list-none pl-1">
+      <ul className="py-4 text-[14px] text-neutral-90 leading-7 list-none pl-1">
         {rulesArray.map((rule: string, i: number) => (
           <li key={`${rule}-${i}`}>{rule}</li>
         ))}
