40c435b8766fdf9df15403d9c301ac832ae440a6
HyoEun Sim <hyoeun5000@naver.com>
2024-03-14 16:27:18 +0900

    style: matchcontainer padding 수정

--- src/app/(providers)/(root)/matches/[matchId]/_components/OverlayBackground.tsx
+++ src/app/(providers)/(root)/matches/[matchId]/_components/OverlayBackground.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 function OverlayBackground({ children }: PropsWithChildren) {
   return (
-    <div className="transform rounded-t-3xl sm:mt-[-50px] mt-[-20px] bg-white w-full p-5 shadow-inner">
+    <div className="transform rounded-t-3xl sm:mt-[-50px] mt-[-20px] bg-white w-full p-5 shadow-lg shadow-slate-900/20 shadow-t-2 shadow-r-[3px]">
       {children}
     </div>
   );
