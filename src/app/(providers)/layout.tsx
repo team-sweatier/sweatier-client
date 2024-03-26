66565c30e4e545fe824a5cdc867826812823aeef
HyoEun Sim <hyoeun5000@naver.com>
2024-03-26 18:54:27 +0900

    docs: 주요 기능 및 게시물 페이지 시현 화면 update

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,7 +1,6 @@
 import { AuthProvider } from "@/contexts/auth.context";
 import { ProfileProvider } from "@/contexts/profile.context";
 import { TiersProvider } from "@/contexts/tiers.context";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactQueryProvider from "./_providers/reactQuery.providers";
 
 function ProvidersLayout({ children }: { children: React.ReactNode }) {
@@ -12,7 +11,6 @@ function ProvidersLayout({ children }: { children: React.ReactNode }) {
           <TiersProvider>{children}</TiersProvider>
         </ProfileProvider>
       </AuthProvider>
-      <ReactQueryDevtools />
     </ReactQueryProvider>
   );
 }
